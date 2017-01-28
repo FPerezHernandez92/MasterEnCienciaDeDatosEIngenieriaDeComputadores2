4908e8a46c5a5830a7b1a1f99b7bc9e3f8d467a7
PacoPollos <herpefran92@MacBook-Pro-de-Francisco.local>
2017-01-28 17:49:15 +0100

    Minería de Datos: Aspecto Avanzados práctica 1 de datos imbalanceados

---
+++ 3 Mineria de Datos, Aspectos Avanzados/Clasificacion con conjuntos de datos no balanceados, no equilibrados/imbalanceado.R
@@ -0,0 +1,141 @@
+setwd("~/Dropbox/zMaster/zRStudio/Master-en-Ciencia-De-Datos-e-Ingeniería-de-Computadores2/3 Mineria de Datos, Aspectos Avanzados/Clasificacion con conjuntos de datos no balanceados, no equilibrados")
+
+#Implementación y evaluación de técnicas de clasificación imbalanceada
+#Leemos el dataset subclus
+subclus <- read.table("subclus.txt", sep=",")
+colnames(subclus) <- c("Att1", "Att2", "Class")
+
+#Determinar el radio de imbalanceamiento
+unique(subclus$Class)
+nClass0 <- sum(subclus$Class == 0)
+nClass1 <- sum(subclus$Class == 1)
+IR <- nClass1 / nClass0
+IR #Por cada ejemplo positivo hay 5 negativos
+
+#Visualizamos la distribución de los datos
+plot(subclus$Att1, subclus$Att2)
+points(subclus[subclus$Class==0,1],subclus[subclus$Class==0,2],col="red")
+points(subclus[subclus$Class==1,1],subclus[subclus$Class==1,2],col="blue")  
+
+#Dividimos el dataset en 5 partes para la validación cruzada
+pos <- (1:dim(subclus)[1])[subclus$Class==0]
+neg <- (1:dim(subclus)[1])[subclus$Class==1]
+#Hacemos las divisiones en los 5 conjuntos de cada clase
+CVperm_pos <- matrix(sample(pos,length(pos)), ncol=5, byrow=T)
+CVperm_neg <- matrix(sample(neg,length(neg)), ncol=5, byrow=T)
+#Unimos las dos clases
+CVperm <- rbind(CVperm_pos, CVperm_neg)
+
+#Llevamos a cabo el 3NN
+library(class)
+knn.pred = NULL
+set.seed(1234)
+for( i in 1:5){
+  predictions <- knn(subclus[-CVperm[,i], -3], subclus[CVperm[,i], -3], subclus[-CVperm[,i], 3], k = 3)
+  knn.pred <- c(knn.pred, predictions)
+}
+acc <- sum((subclus$Class[as.vector(CVperm)] == 0 & knn.pred == 1) 
+           | (subclus$Class[as.vector(CVperm)] == 1 & knn.pred == 2)) / (nClass0 + nClass1) 
+acc
+#parece que se obitene un buen rendimiento ya que tenemos un accuracy del 0.93
+
+
+tnr <- sum(subclus$Class[as.vector(CVperm)] == 1 & knn.pred == 2) / nClass1 
+tnr
+#es bueno para la clase negativa al obtener un 0.972
+tpr <- sum(subclus$Class[as.vector(CVperm)] == 0 & knn.pred == 1) / nClass0 
+tpr
+#no es tan bueno para la case positiva ya que obtiene un 0.76
+gmean <- sqrt(tpr * tnr) # no es tan bueno como decia el accuracy 
+gmean
+#Obtenemos de media un 0.85
+
+# 1. Vamos a aplicar ROS, de forma que replicaremos los ejemplos positivos
+knn.pred = NULL
+set.seed(1234)
+for( i in 1:5){
+  train <- subclus[-CVperm[,i], -3]
+  classes.train <- subclus[-CVperm[,i], 3] 
+  test  <- subclus[CVperm[,i], -3]
+  #Aleatoriamente se hace oversample de la clase minoritaria (class 0)
+  minority.indices <- (1:dim(train)[1])[classes.train == 0]
+  to.add <- dim(train)[1] - 2 * length(minority.indices)
+  duplicate <- sample(minority.indices, to.add, replace = T)
+  for( j in 1:length(duplicate)){
+    train <- rbind(train, train[duplicate[j],])
+    classes.train <- c(classes.train, 0)
+  }  
+  # use the modified training set to make predictions
+  predictions <-  knn(train, test, classes.train, k = 3)
+  knn.pred <- c(knn.pred, predictions)
+} 
+
+tpr.ROS <- sum(subclus$Class[as.vector(CVperm)] == 0 & knn.pred == 1) / nClass0
+tpr.ROS 
+#Hemos pasado de un 0.76 a un 0.92 mejorando mucho en la clase positiva
+tnr.ROS <- sum(subclus$Class[as.vector(CVperm)] == 1 & knn.pred == 2) / nClass1 
+tnr.ROS
+#Hemos pasado de un 0.97 a un 0.90 en la clase negativa
+gmean.ROS <- sqrt(tpr.ROS * tnr.ROS) 
+gmean.ROS
+#La media ha pasado de 0.85 a 0.91, mejorando
+
+# 2. Vamos a aplicar RUS, de forma que quito elementos de la clase mayoritaria
+set.seed(1234)
+knn.pred = NULL
+for( i in 1:5){
+  train <- subclus[-CVperm[,i], -3]
+  classes.train <- subclus[-CVperm[,i], 3] 
+  test  <- subclus[CVperm[,i], -3]
+  #Aleatoriamente aplicamos undersample a la clase minoritaria (class 1)
+  majority.indices <- (1:dim(train)[1])[classes.train == 1]
+  to.remove <- 2* length(majority.indices) - dim(train)[1]
+  remove <- sample(majority.indices, to.remove, replace = F)
+  train <- train[-remove,] 
+  classes.train <- classes.train[-remove]
+  #Uso el training set modificado para hacer las predicciones
+  predictions <-  knn(train, test, classes.train, k = 3)
+  knn.pred <- c(knn.pred, predictions)
+} 
+tpr.RUS <- sum(subclus$Class[as.vector(CVperm)] == 0 & knn.pred == 1) / nClass0
+tpr.RUS
+#Hemos pasado de 0.92 a 0.93
+tnr.RUS <- sum(subclus$Class[as.vector(CVperm)] == 1 & knn.pred == 2) / nClass1
+tnr.RUS
+#Hemos pasado de 0.90 a 0.80
+gmean.RUS <- sqrt(tpr.RUS * tnr.RUS) 
+gmean.RUS
+#Hemos bajado el rendimiento con respecto a ROS, ya que ha bajado de 0.91 a 0.86,
+#pero se ha mejorado al inicial sin aplicar ninguna técnica
+
+#Visualizamos como quedaría la técnica RUS en el dataset
+subclus.RUS <- subclus
+majority.indices <- (1:dim(subclus.RUS)[1])[subclus.RUS$Class == 1]
+to.remove <- 2 * length(majority.indices) - dim(subclus.RUS)[1]
+remove <- sample(majority.indices, to.remove, replace = F)
+subclus.RUS <- subclus.RUS[-remove,] 
+
+plot(subclus.RUS$Att1, subclus.RUS$Att2)
+points(subclus.RUS[subclus.RUS$Class==0,1],subclus.RUS[subclus.RUS$Class==0,2],col="red")
+points(subclus.RUS[subclus.RUS$Class==1,1],subclus.RUS[subclus.RUS$Class==1,2],col="blue") 
+
+
+# 1.4.1 Función de distancia
+distance <- function(i, j, data){
+  sum <- 0
+  for(f in 1:dim(data)[2]){
+    if(is.factor(data[,f])){ # nominal feature
+      if(data[i,f] != data[j,f]){
+        sum <- sum + 1
+      }
+    } else {
+      sum <- sum + (data[i,f] - data[j,f]) * (data[i,f] - data[j,f])
+    }
+  }
+  sum <- sqrt(sum)
+  return(sum)
+}
+#TAREAS:
+#hacer getinstance. hacer syntheticInstance para crear instancias. comparar los resultados de este nuevo train en ros y rus. 
+#(opcional) hacer implementación de smote
+
